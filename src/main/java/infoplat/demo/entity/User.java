6f0c3d30bf58c0620f04b6e6ed68314b7a0d3066
IzumSakai1995 <IzumiSakai@gmail1995.com>
2019-01-16 14:18:06 +0800

    update

--- src/main/java/infoplat/demo/entity/User.java
+++ src/main/java/infoplat/demo/entity/User.java
@@ -1,11 +1,15 @@
 package infoplat.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int iAdminId;
     private String sName;
@@ -15,42 +19,7 @@ public class User {
     private int iInsertAdmin;
     private Date dtUpdate;
     private int iUpdateAdmin;
-    private int iRole;
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "iAdminId=" + iAdminId +
-                ", sName='" + sName + '\'' +
-                ", sTel='" + sTel + '\'' +
-                ", sPwd='" + sPwd + '\'' +
-                ", dtInsert=" + dtInsert +
-                ", iInsertAdmin=" + iInsertAdmin +
-                ", dtUpdate=" + dtUpdate +
-                ", iUpdateAdmin=" + iUpdateAdmin +
-                ", iRole=" + iRole +
-                '}';
-    }
-
-    public User(){
-
-    }
-
-    public User(int iAdminId, String sName, String sTel, String sPwd, Date dtInsert, int iInsertAdmin, Date dtUpdate, int iUpdateAdmin, int iRole) {
-        this.iAdminId = iAdminId;
-        this.sName = sName;
-        this.sTel = sTel;
-        this.sPwd = sPwd;
-        this.dtInsert = dtInsert;
-        this.iInsertAdmin = iInsertAdmin;
-        this.dtUpdate = dtUpdate;
-        this.iUpdateAdmin = iUpdateAdmin;
-        this.iRole = iRole;
-    }
+    private int iRoleId;
 
 }
-class TestUser{
-    public static void main(String[] args) {
-        User user= new User();
-    }
-}
+
